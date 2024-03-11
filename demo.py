153a92f25d08f343953bd9345db30b8308909c30
Vinit Sontakke <vinitsontakke27@gmail.com>
2024-03-12 00:44:11 +0530

    created multiplication function in demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
 def addition(n, m):
     return  n + m
 
+def multiply(n,m):
+    return  n * m
+
 n = int(input("enter 1st no :"))
 m = int(input("Enter 2nd  no : "))
 print("Sum is ", addition(n,m))
+print("Product is ", multiply(n,m))
 print("Thanks ........")
